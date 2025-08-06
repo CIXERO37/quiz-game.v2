6db41787e236b1c96bd618d9d551db8d656ca025
CIXERO37 <dlol59005@gmail.com>
2025-08-06 16:17:36 +0700

    Merubah gambar asteroid di mini game & ubah timer

--- components/space-dodge.tsx
+++ components/space-dodge.tsx
@@ -49,7 +49,7 @@ const POWER_UPS_CONFIG = {
 }
 
 export default function SpaceDodge({ onComplete }: Props) {
-  const [timeLeft, setTimeLeft] = useState(20)
+  const [timeLeft, setTimeLeft] = useState(25)
   const [score, setScore] = useState(0)
   const [meteors, setMeteors] = useState<Meteor[]>([])
   const [powerUps, setPowerUps] = useState<PowerUp[]>([])
